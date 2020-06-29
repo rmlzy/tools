0dd8fe0637c6550e3aa5d92ef8795aa10738f37c
rmlzy <rmlzy@outlook.com>
2020-06-29 14:28:20 +0800

    feat: update Schedule

--- app/schedule/tophub.js
+++ app/schedule/tophub.js
@@ -14,8 +14,8 @@ class TopHub extends Subscription {
   async subscribe() {
     const { ctx, config } = this;
     try {
-      const minImagePath = path.join(config.baseDir, "app/public/tophub/tech.json");
-      await fs.remove(minImagePath);
+      const cachedPath = path.join(config.baseDir, "app/public/tophub/tech.json");
+      await fs.remove(cachedPath);
       await ctx.service.tophub.fetchNodes("tech");
     } catch (e) {
       ctx.logger.error("抓取今日热榜失败: ", e);
